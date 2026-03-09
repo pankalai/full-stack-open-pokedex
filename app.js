2618cf551e5fc79c8f880d53ffdea9222bec83b5
Anssi Pänkäläinen <pankalai@csc.fi>
2026-03-09 19:26:59 +0200

    exercise 11.6

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+import express, { static as static_m } from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(static_m('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
